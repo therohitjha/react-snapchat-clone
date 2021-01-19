fce4817e8933aaffdcad19290debf02765f27d2d
Rohit Jha <therohitjha@gmail.com>
2021-01-19 14:46:20 +0530

    Updates

---
+++ precache-manifest.512d4aa1e6864baa0a7d814f7425dd94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3687eb2c684541b88378e7d48ac3e8",
+    "url": "/react-snapchat-clone/index.html"
+  },
+  {
+    "revision": "d1ff1f4d6e5ade53df74",
+    "url": "/react-snapchat-clone/static/css/2.dda6c3ea.chunk.css"
+  },
+  {
+    "revision": "1b2f5d234c06f28dbaea",
+    "url": "/react-snapchat-clone/static/css/main.75637886.chunk.css"
+  },
+  {
+    "revision": "d1ff1f4d6e5ade53df74",
+    "url": "/react-snapchat-clone/static/js/2.e54365be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-snapchat-clone/static/js/2.e54365be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2f5d234c06f28dbaea",
+    "url": "/react-snapchat-clone/static/js/main.78675b4e.chunk.js"
+  },
+  {
+    "revision": "3500ff03da3d7810cc2a",
+    "url": "/react-snapchat-clone/static/js/runtime-main.d5e13c5d.js"
+  }
+]);
